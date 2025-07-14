55400727d8b931dfdef182ff450c08a07a9c2813
Matthew Ntsiful <matthew.ntsiful@gmail.com>
2025-07-14 22:50:48 +0000

    CICP Pilpeline Updated

--- update-manifests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Set your Docker registry (replace with your registry)
-REGISTRY="your-registry.com/your-username"
-
-# Update image references in all YAML files
-find microservices-demo/kubernetes-manifests -name "*.yaml" -exec sed -i '' "s|image: \([^/]*\)$|image: ${REGISTRY}/\1:latest|g" {} \;
-
-echo "Updated all image references to use registry: ${REGISTRY}"
